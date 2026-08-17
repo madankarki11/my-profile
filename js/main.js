9a0c5cd2b49c19a142d4781d0c03244be2ed266d
madankarki11 <jerrykarki59@gmail.com>
2026-08-17 22:28:19 +0545

    modification

--- js/main.js
+++ js/main.js
@@ -35,3 +35,40 @@ const yearEl = document.getElementById('year');
 if (yearEl) {
   yearEl.textContent = new Date().getFullYear();
 }
+
+// Swap portrait on hover with smooth fade
+const profileImages = document.querySelectorAll('.hero__image, .sidebar__profile-img');
+const hoverImageCandidates = [
+  'file:///C:/Users/ACER/OneDrive/Desktop/my%20profile/this%20is%20my%20image.jpg',
+  'file:///C:/Users/ACER/OneDrive/Desktop/my%20profile/this%20is%20my%20image.jpg',
+  'file:///C:/Users/ACER/OneDrive/Desktop/my%20profile/This%20is%20my%20image1.jpg'
+];
+const hoverImageSrc = hoverImageCandidates[0];
+
+profileImages.forEach((img) => {
+  const originalSrc = img.getAttribute('src');
+
+  if (!originalSrc) return;
+
+  img.dataset.originalSrc = originalSrc;
+
+  const changeImage = (targetSrc) => {
+    img.style.opacity = '0';
+    setTimeout(() => {
+      img.setAttribute('src', targetSrc);
+      img.style.opacity = '1';
+    }, 170);
+  };
+
+  img.addEventListener('mouseenter', () => {
+    if (img.getAttribute('src') !== hoverImageSrc) {
+      changeImage(hoverImageSrc);
+    }
+  });
+
+  img.addEventListener('mouseleave', () => {
+    if (img.getAttribute('src') !== originalSrc) {
+      changeImage(originalSrc);
+    }
+  });
+});
